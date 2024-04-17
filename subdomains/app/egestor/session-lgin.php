5ccad5737030b5e860408dd4c28b1d96a1b875b6
Pedro Miguel Correa <pedro@tycho-techs.com.br>
2024-04-17 05:28:03 -0300

    Delete subdomains directory
    Signed-off-by: Pedro Miguel Correa <pedro@tycho-techs.com.br>

--- subdomains/app/egestor/session-lgin.php
+++
@@ -1,8 +0,0 @@
-<?php
-        session_start(); 
-        
-        if(!isset($_SESSION['logged_in'])){
-            header('Location: https://app.sweetadm.com.br/egestor/login.php'); 
-            exit;
-        }
-    ?>
--- subdomains/bi/PowerBI-Developer-Samples-master/PowerBI-Developer-Samples-master/PowerShell Scripts/Create-Workspace.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host
-
-Connect-PowerBIServiceAccount | Out-Null
-
-$workspaceName = "Test Workspace 1"
-
-$workspace = New-PowerBIGroup -Name $workspaceName
-
-$workspace | select *
